0b71b43ba9901c45d1717ba083a8df68eb7286d4
David Smiley <DGSmiley77@gmail.com>
2016-10-12 10:59:55 -0400

    first commit

---
+++ src/Dojo/GCDojo.java
@@ -0,0 +1,13 @@
+package Dojo;
+
+import java.util.Scanner;
+
+public class GCDojo {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to Grant Chirpus's Code Dojo! We offer a variety of classes that pave the way for an exciting career in technology. Enter 'enroll' at any time to join a class");
+
+	}
+
+}
