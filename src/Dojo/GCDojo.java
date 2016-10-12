97471e1100b2ed974e4138d2ae8b364766e458cb
David Smiley <DGSmiley77@gmail.com>
2016-10-12 11:20:11 -0400

    2nd commit

--- src/Dojo/GCDojo.java
+++ src/Dojo/GCDojo.java
@@ -6,7 +6,14 @@ public class GCDojo {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Welcome to Grant Chirpus's Code Dojo! We offer a variety of classes that pave the way for an exciting career in technology. Enter 'enroll' at any time to join a class");
+		System.out.println("Welcome to Grant Chirpus's Code Dojo! We offer a variety of classes that pave the way for an exciting career in technology. Enter 'enroll' at any time to join a class." + "\n");
+		
+		System.out.println("Our classes include: " + "\n");
+		System.out.println("Hard Skills: ");
+		System.out.print("Java" + "\n" + "JavaScript" + "\n" + "Web Development" + "\n" + "Software Testing");
+		System.out.println("\n");
+		System.out.println("Soft Skills: ");
+		System.out.print("Interviewing" + "\n" + "Networking" + "\n");
 
 	}
 
